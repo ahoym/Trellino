6aafca70592a023e56c0dfeef01b2589ff65b1b5
Malcolm Ahoy <colmahoy@gmail.com>
2014-03-17 17:47:41 -0700

    Add css to cards

--- app/assets/javascripts/views/cards_show.js
+++ app/assets/javascripts/views/cards_show.js
@@ -1,5 +1,8 @@
 window.Trellino.Views.CardsShowView = Backbone.View.extend ({
 	template: JST["cards/show"],
+	className: "card",
+	
+	events: { "mouseover div.card": "showDeleteButton" },
 
 	initialize: function (options) {
 		this.card = options.card
@@ -10,5 +13,19 @@ window.Trellino.Views.CardsShowView = Backbone.View.extend ({
 		this.$el.html(renderedContent);
 		
 		return this;
+	},
+	
+	showDeleteButton: function (event) {
+		console.log("PEW")
+		$(event.currentTarget).hover(
+			function() {
+				$(this).fadeIn(500);
+				$(this).append("<button id='delete'>X</button>");
+			},
+			function() {
+				$(this).remove("<button id='delete'>X</button>");
+			}
+		)
 	}
+	
 });
