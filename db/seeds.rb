c79f82b4f995e47ccedb8a63b4be67d766184490
Malcolm Ahoy <colmahoy@gmail.com>
2014-04-15 14:08:53 -0700

    Add styling to sign-up and log-in pages, populate more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # 
 users = User.create!([
-  {email: 'a', password: "a"}
+  {email: 'demo@user.com', password: 'demo'}
 ])
 
 boards = Board.create!([
-  {title: "Home"}, #1
-  {title: "Work"} #2
+  {title: "Home"},
+  {title: "Work"}
 ])
 
-board_assignments = BoardAssignment.create!([
-  {user_id: 1, board_id: 1},
-  {user_id: 1, board_id: 2}
-])
+# board_assignments = BoardAssignment.create!([
+#   {user_id: 1, board_id: 1},
+#   {user_id: 1, board_id: 2}
+# ])
 
 lists = List.create!([
   #Home: board 1
@@ -35,8 +35,13 @@ cards = Card.create!([
   #To Do: List 1 on Home
   {list_id: 1, rank: 1, title: "Clean dishes"},
   {list_id: 1, rank: 2, title: "Mop kitchen"},
+  {list_id: 1, rank: 3, title: "Vacuum the house"},
+  {list_id: 1, rank: 4, title: "Change the oil for the car"},
+  {list_id: 1, rank: 5, title: "Do Homework"},
+  
   #Doing: List 2 on Home
   {list_id: 2, rank: 1, title: "Fix back door"},
+  {list_id: 2, rank: 2, title: "Mop kitchen"},
   #Done: List 3 on Home
   {list_id: 3, rank: 1, title: "Water plants"},
   {list_id: 3, rank: 2, title: "Fold sheets"},
@@ -44,10 +49,20 @@ cards = Card.create!([
   #Urgent: List 4 on Work
   {list_id: 4, rank: 1, title: "Prepare earnings spreadsheet"},
   {list_id: 4, rank: 2, title: "Build sales presentation"},
+  {list_id: 4, rank: 3, title: "Handle errors on submission"},
+  {list_id: 4, rank: 4, title: "Make Cards editable"},
+  {list_id: 4, rank: 6, title: "Enable card descriptions"},
+  {list_id: 4, rank: 7, title: "GET A JOB!"},
+  
   #Important: List 5 on Work
   {list_id: 5, rank: 1, title: "Find new accountant"},
   {list_id: 5, rank: 2, title: "Look into bigger office space"},
+  {list_id: 5, rank: 3, title: "Practice writing unit tests"},
+  {list_id: 5, rank: 5, title: "GET A JOB!"},
+  
   #Optional: List 6 on Work
   {list_id: 6, rank: 1, title: "Review workflows"},
-  {list_id: 6, rank: 2, title: "Redecorate conference room"}
+  {list_id: 6, rank: 2, title: "Redecorate conference room"},
+  {list_id: 6, rank: 2, title: "Create an About Page"}
+  {list_id: 6, rank: 2, title: "Create a Contacts Page"}
 ])
